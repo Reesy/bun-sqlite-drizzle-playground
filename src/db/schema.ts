eede320910ad6e02bfe6e8edd9859d31d9d955a9
James Rees <james.rees@outlook.com>
2024-11-10 11:44:38 +0900

    Added schema file

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,1 +1,8 @@
-//placeholder for schema file
+import { int, sqliteTable, text } from "drizzle-orm/sqlite-core";
+
+export const usersTable = sqliteTable("users_table", {
+  id: int().primaryKey({ autoIncrement: true }),
+  name: text().notNull(),
+  age: int().notNull(),
+  email: text().notNull().unique(),
+});
